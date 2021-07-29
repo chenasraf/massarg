654b85ae32f7fb1e8a8be4db467d28a85d39965f
Chen Asraf <chenasrafil@gmail.com>
2021-07-29 22:40:50 +0300

    improve required message

--- src/errors.ts
+++ src/errors.ts
@@ -3,7 +3,11 @@ export class RequiredError extends Error {
   cmdName!: string
 
   constructor(fieldName: string, cmdName: string) {
-    super(`${fieldName} is required, but was not defined` + (cmdName !== "all" ? ` for ${cmdName}` : ""))
+    super(
+      `Option: \`${fieldName}\` is required${
+        cmdName !== "all" ? ` for command: \`${cmdName}\`` : ""
+      }, but was not defined. Try using: \`--${fieldName} {value}\``
+    )
     this.fieldName = fieldName
     this.cmdName = cmdName
   }
