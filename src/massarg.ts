73c1ad95913ab3cba5ca304e2759a9fd76212ccc
Chen Asraf <casraf@pm.me>
2023-11-19 01:58:41 +0200

    feat: array & typed options

--- src/massarg.ts
+++ src/massarg.ts
@@ -1,16 +1,18 @@
 import MassargCommand, { ArgsObject, CommandConfig } from "./command"
 
-type MinimalCommandConfig<Args extends ArgsObject> = Omit<CommandConfig<Args>, "aliases" | "run">
+type MinimalCommandConfig<Args extends ArgsObject> = Omit<CommandConfig<Args>, "aliases" | "run"> &
+  Partial<Pick<CommandConfig<Args>, "aliases" | "run">>
 
 export default class Massarg<Args extends ArgsObject = ArgsObject> extends MassargCommand<Args> {
   constructor(options: MinimalCommandConfig<Args>) {
     // TODO consider re-using name and description for general help, and pass them to super
     super({
-      ...options,
       aliases: [],
       run: () => {
-        throw new Error("Massarg is not a command")
+        console.log(this.helpString())
+        // throw new Error("No main command provided")
       },
+      ...options,
     })
   }
 }
--- src/option.ts
+++ src/option.ts
@@ -9,10 +9,32 @@ export const OptionConfig = <T extends z.ZodType>(type: T) =>
     defaultValue: z.any().optional(),
     aliases: z.string().array(),
     parse: z.function().args(z.string()).returns(type).optional(),
+    array: z.boolean().optional(),
   })
 export type OptionConfig<T = unknown> = z.infer<ReturnType<typeof OptionConfig<z.ZodType<T>>>>
 
-export type OptionType = "string" | "number" | "boolean"
+export const TypedOptionConfig = <T extends z.ZodType>(type: T) =>
+  OptionConfig(type).merge(
+    z.object({
+      type: z.enum(["string", "number", "boolean"]).optional(),
+    }),
+  )
+export type TypedOptionConfig<T = unknown> = z.infer<ReturnType<typeof TypedOptionConfig<z.ZodType<T>>>>
+
+export const ArrayOptionConfig = <T extends z.ZodType>(type: T) =>
+  TypedOptionConfig(z.array(type)).merge(
+    z.object({
+      defaultValue: z.array(type).optional(),
+    }),
+  )
+export type ArrayOptionConfig<T = unknown> = z.infer<ReturnType<typeof ArrayOptionConfig<z.ZodType<T>>>>
+
+const OPT_FULL_PREFIX = "--"
+const OPT_SHORT_PREFIX = "-"
+const NEGATE_FULL_PREFIX = "no-"
+const NEGATE_SHORT_PREFIX = "^"
+
+export type ArgvValue<T> = { argv: string[]; value: T; key: string }
 
 export default class MassargOption<T = unknown> {
   name: string
@@ -20,6 +42,7 @@ export default class MassargOption<T = unknown> {
   defaultValue?: T
   aliases: string[]
   parse: (value: string) => T
+  isArray: boolean
 
   constructor(options: OptionConfig<T>) {
     OptionConfig(z.any()).parse(options)
@@ -28,9 +51,20 @@ export default class MassargOption<T = unknown> {
     this.defaultValue = options.defaultValue
     this.aliases = options.aliases
     this.parse = options.parse ?? ((x) => x as unknown as T)
+    this.isArray = options.array ?? false
   }
 
-  valueFromArgv(argv: string[]): { argv: string[]; value: T; key: string } {
+  static fromTypedConfig<T = unknown>(config: TypedOptionConfig<T>): MassargOption<T> {
+    switch (config.type) {
+      case "number":
+        return new MassargNumber(config as OptionConfig<number>) as MassargOption<T>
+      case "boolean":
+        return new MassargFlag(config) as MassargOption<T>
+    }
+    return new MassargOption(config as OptionConfig<T>)
+  }
+
+  valueFromArgv(argv: string[]): ArgvValue<T> {
     // TODO: support --option=value
     argv.shift()
     try {
@@ -47,6 +81,36 @@ export default class MassargOption<T = unknown> {
       throw e
     }
   }
+
+  helpString(): string {
+    const aliases = this.aliases.length ? `|${this.aliases.join("|-")}` : ""
+    return `--${this.name}${aliases} ${this.description}`
+  }
+
+  _match(arg: string): boolean {
+    // full prefix
+    if (arg.startsWith(OPT_FULL_PREFIX)) {
+      // negate full prefix
+      if (arg.startsWith(`--${NEGATE_FULL_PREFIX}`)) {
+        return this.name === arg.slice(`--${NEGATE_FULL_PREFIX}`.length)
+      }
+      return this.name === arg.slice(OPT_FULL_PREFIX.length)
+    }
+    // short prefix
+    if (arg.startsWith(OPT_SHORT_PREFIX) || arg.startsWith(NEGATE_SHORT_PREFIX)) {
+      return this.aliases.includes(arg.slice(OPT_SHORT_PREFIX.length))
+    }
+    // negate short prefix
+    if (arg.startsWith(NEGATE_SHORT_PREFIX)) {
+      return this.aliases.includes(arg.slice(NEGATE_SHORT_PREFIX.length))
+    }
+    // no prefix
+    return false
+  }
+
+  _isOption(arg: string): boolean {
+    return arg.startsWith(OPT_FULL_PREFIX) || arg.startsWith(OPT_SHORT_PREFIX) || arg.startsWith(NEGATE_SHORT_PREFIX)
+  }
 }
 
 export class MassargNumber extends MassargOption<number> {
@@ -57,7 +121,7 @@ export class MassargNumber extends MassargOption<number> {
     })
   }
 
-  valueFromArgv(argv: string[]): { argv: string[]; value: number; key: string } {
+  valueFromArgv(argv: string[]): ArgvValue<number> {
     try {
       const { argv: _argv, value } = super.valueFromArgv(argv)
       if (isNaN(value)) {
@@ -89,9 +153,9 @@ export class MassargFlag extends MassargOption<boolean> {
     })
   }
 
-  valueFromArgv(argv: string[]): { argv: string[]; value: boolean; key: string } {
+  valueFromArgv(argv: string[]): ArgvValue<boolean> {
     try {
-      const isNegation = argv[0]?.startsWith("-!")
+      const isNegation = argv[0]?.startsWith("^")
       argv.shift()
       if (isNegation) {
         return { key: this.name, value: false, argv }
