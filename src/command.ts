a0468ab1b523c23946c0219648d00d568f186f64
Chen Asraf <casraf@pm.me>
2023-11-19 01:58:41 +0200

    feat: array & typed options

--- src/command.ts
+++ src/command.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 import { ValidationError } from "./error"
-import MassargOption, { MassargFlag, MassargNumber, OptionConfig, OptionType } from "./option"
-import { isZodError } from "./utils"
+import MassargOption, { MassargFlag, MassargNumber, OptionConfig, TypedOptionConfig } from "./option"
+import { generateCommandsHelpTable, generateOptionsHelpTable, isZodError } from "./utils"
 
 export const CommandConfig = <RunArgs extends z.ZodType>(args: RunArgs) =>
   z.object({
@@ -18,14 +18,16 @@ export type CommandConfig<T = unknown> = z.infer<ReturnType<typeof CommandConfig
 
 export type ArgsObject = Record<string, unknown>
 
+// export type RunFn<Args extends ArgsObject> = (options: Args) => Promise<void> | void
+
 export default class MassargCommand<Args extends ArgsObject = ArgsObject> {
   name: string
   description: string
-  private aliases: string[]
+  aliases: string[]
   private _run?: (options: Args) => Promise<void> | void
-  private options: MassargOption[] = []
-  private commands: MassargCommand<any>[] = []
-  private args: Partial<Args> = {}
+  options: MassargOption[] = []
+  commands: MassargCommand<any>[] = []
+  args: Partial<Args> = {}
 
   constructor(options: CommandConfig<Args>) {
     CommandConfig(z.any()).parse(options)
@@ -35,9 +37,9 @@ export default class MassargCommand<Args extends ArgsObject = ArgsObject> {
     this._run = options.run
   }
 
-  command(config: CommandConfig<Args>): MassargCommand<Args>
-  command(config: MassargCommand<Args>): MassargCommand<Args>
-  command(config: CommandConfig<Args> | MassargCommand<Args>): MassargCommand<Args> {
+  command<A extends ArgsObject = Args>(config: CommandConfig<A>): MassargCommand<Args>
+  command<A extends ArgsObject = Args>(config: MassargCommand<A>): MassargCommand<Args>
+  command<A extends ArgsObject = Args>(config: CommandConfig<A> | MassargCommand<A>): MassargCommand<Args> {
     try {
       const command = config instanceof MassargCommand ? config : new MassargCommand(config)
       this.commands.push(command)
@@ -55,23 +57,10 @@ export default class MassargCommand<Args extends ArgsObject = ArgsObject> {
   }
 
   option<T = string>(config: MassargOption<T>): MassargCommand<Args>
-  option<T = string>(config: OptionConfig<T> & { type?: OptionType }): MassargCommand<Args>
-  option<T = string>(config: (OptionConfig<T> & { type?: OptionType }) | MassargOption<T>): MassargCommand<Args> {
-    const factory = () => {
-      if (!("type" in config)) {
-        return new MassargOption(config as OptionConfig<T>)
-      }
-      switch (config.type) {
-        case "string":
-          return new MassargOption<string>(config as OptionConfig<string>)
-        case "number":
-          return new MassargNumber(config as OptionConfig<number>)
-        case "boolean":
-          return new MassargFlag(config)
-      }
-    }
+  option<T = string>(config: TypedOptionConfig<T>): MassargCommand<Args>
+  option<T = string>(config: TypedOptionConfig<T> | MassargOption<T>): MassargCommand<Args> {
     try {
-      const option = config instanceof MassargOption ? config : factory()
+      const option = config instanceof MassargOption ? config : MassargOption.fromTypedConfig(config)
       this.options.push(option as MassargOption)
       return this
     } catch (e) {
@@ -100,7 +89,8 @@ export default class MassargCommand<Args extends ArgsObject = ArgsObject> {
     while (_argv.length) {
       const arg = _argv.shift()!
       console.log("parsing:", arg, _argv)
-      if (arg.startsWith("-")) {
+      const found = this.options.some((o) => o._isOption(arg))
+      if (found) {
         console.log("option:", arg, _argv)
         _argv = this.parseOption(arg, _argv)
         continue
@@ -121,15 +111,21 @@ export default class MassargCommand<Args extends ArgsObject = ArgsObject> {
   }
 
   private parseOption(arg: string, argv: string[]): string[] {
-    const option = this.options.find(
-      (o) => (arg.startsWith("--") && o.name === arg.slice(2)) || o.aliases.includes(arg.slice(1)),
-    )
+    const option = this.options.find((o) => o._match(arg))
+
     if (!option) {
       // TODO create custom error object
       throw new Error(`Unknown option ${arg}`)
     }
     const res = option.valueFromArgv([arg, ...argv])
-    this.args[res.key as keyof Args] = res.value as Args[keyof Args]
+    console.log("option class name", option.constructor.name)
+    if (option.isArray) {
+      this.args[res.key as keyof Args] ??= [] as Args[keyof Args]
+      const _a = this.args[res.key as keyof Args] as unknown[]
+      _a.push(res.value) as Args[keyof Args]
+    } else {
+      this.args[res.key as keyof Args] = res.value as Args[keyof Args]
+    }
     console.log("option response:", { value: res.value, argv: res.argv })
     return res.argv
   }
@@ -138,6 +134,22 @@ export default class MassargCommand<Args extends ArgsObject = ArgsObject> {
     console.log("getArgs:", this.name)
     console.log(argv)
     return {} as Args
+  }
+
+  helpString(): string {
+    const options = generateOptionsHelpTable(this.options)
+    const commands = generateCommandsHelpTable(this.commands)
+    return [
+      `${this.name} - ${this.description}`,
+      commands.length && "",
+      commands.length && `Commands for ${this.name}:`,
+      commands.length && commands,
+      options.length && "",
+      options.length && `Options for ${this.name}:`,
+      options.length && options,
+    ]
+      .filter((s) => typeof s === "string")
+      .join("\n")
   }
 }
 
--- src/example.ts
+++ src/example.ts
@@ -1,37 +1,84 @@
 import { massarg } from "."
 import MassargCommand from "./command"
+import { ParseError } from "./error"
 
 type A = { test: boolean }
 const args = massarg<A>({
   name: "my-cli",
   description: "This is an example CLI",
 })
-  .main((opts) => {
-    console.log("Main command - printing all opts")
-    console.log(opts)
-  })
+  // .main((opts) => {
+  //   console.log("Main command - printing all opts")
+  //   console.log(opts)
+  // })
   .command(
-    new MassargCommand<A>({
-      name: "command",
-      description: "Example command",
-      aliases: ["c"],
+    massarg<{ component: string }>({
+      name: "add",
+      description: "Add a component",
+      aliases: ["a"],
       run: (opts) => {
-        console.log("`command` Command - printing all opts")
-        console.log(opts)
+        console.log("Adding component", opts.component)
+      },
+    })
+      .option({
+        name: "component",
+        description: "Component to add",
+        aliases: ["c"],
+        // aliases: "" as never,
+      })
+      .option({
+        name: "classes",
+        description: "Classes to add",
+        aliases: ["l"],
+        array: true,
+      })
+      .option({
+        name: "custom",
+        description: "Custom option",
+        aliases: ["x"],
+        parse: (value) => {
+          const asNumber = Number(value)
+          if (isNaN(asNumber)) {
+            throw new ParseError({
+              path: ["custom"],
+              message: "Custom option must be a number",
+              code: "invalid_number",
+            })
+          }
+          return {
+            value: asNumber,
+            half: asNumber / 2,
+            double: asNumber * 2,
+          }
+        },
+      }),
+  )
+  .command(
+    new MassargCommand<{ component: string }>({
+      name: "remove",
+      description: "Remove a component",
+      aliases: ["r"],
+      run: (opts) => {
+        console.log("Removing component", opts.component)
       },
     }).option({
-      name: "command-option",
-      description: "Example command option",
-      aliases: ["o"],
+      name: "component",
+      description: "Component to remove",
+      aliases: ["c"],
       // aliases: "" as never,
     }),
   )
+  .option({
+    name: "bool",
+    description: "Example number option",
+    aliases: ["b"],
+    type: "boolean",
+  })
   .option({
     name: "number",
     description: "Example number option",
     aliases: ["n"],
     type: "number",
-    parse: (s) => parseFloat(s),
   })
 
 const opts = args.getArgs(process.argv.slice(2))
