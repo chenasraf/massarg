e34b8f2d5fda6b4ed8aff7a5e0d77761808ec40b
Chen Asraf <casraf@pm.me>
2023-11-19 12:55:20 +0200

    feat: help generator

--- src/example.ts
+++ src/example.ts
@@ -3,6 +3,62 @@ import MassargCommand from "./command"
 import { ParseError } from "./error"
 
 type A = { test: boolean }
+const addCmd = massarg<{ component: string }>({
+  name: "add",
+  description: "Add a component",
+  aliases: ["a"],
+  // run: (opts, parser) => {
+  //   parser.printHelp()
+  //   console.log("Adding component", opts.component)
+  // },
+})
+  .option({
+    name: "component",
+    description:
+      "Component to add. Ut consectetur eu et occaecat enim magna amet eiusmod laboris deserunt proident culpa nulla ipsum adipiscing ullamco laboris sed est",
+    aliases: ["c"],
+    // aliases: "" as never,
+  })
+  .option({
+    name: "classes",
+    description: "Classes to add",
+    aliases: ["l"],
+    array: true,
+  })
+  .option({
+    name: "custom",
+    description: "Custom option",
+    aliases: ["x"],
+    parse: (value) => {
+      const asNumber = Number(value)
+      if (isNaN(asNumber)) {
+        throw new ParseError({
+          path: ["custom"],
+          message: "Custom option must be a number",
+          code: "invalid_number",
+        })
+      }
+      return {
+        value: asNumber,
+        half: asNumber / 2,
+        double: asNumber * 2,
+      }
+    },
+  })
+
+const removeCmd = new MassargCommand<{ component: string }>({
+  name: "remove",
+  description: "Remove a component",
+  aliases: ["r"],
+  run: (opts) => {
+    console.log("Removing component", opts.component)
+  },
+}).option({
+  name: "component",
+  description: "Component to remove",
+  aliases: ["c"],
+})
+
 const args = massarg<A>({
   name: "my-cli",
   description: "This is an example CLI",
@@ -12,68 +68,12 @@ const args = massarg<A>({
     console.log(opts, "\n")
     parser.printHelp()
   })
-  .command(
-    massarg<{ component: string }>({
-      name: "add",
-      description: "Add a component",
-      aliases: ["a"],
-      run: (opts) => {
-        console.log("Adding component", opts.component)
-      },
-    })
-      .option({
-        name: "component",
-        description: "Component to add",
-        aliases: ["c"],
-        // aliases: "" as never,
-      })
-      .option({
-        name: "classes",
-        description: "Classes to add",
-        aliases: ["l"],
-        array: true,
-      })
-      .option({
-        name: "custom",
-        description: "Custom option",
-        aliases: ["x"],
-        parse: (value) => {
-          const asNumber = Number(value)
-          if (isNaN(asNumber)) {
-            throw new ParseError({
-              path: ["custom"],
-              message: "Custom option must be a number",
-              code: "invalid_number",
-            })
-          }
-          return {
-            value: asNumber,
-            half: asNumber / 2,
-            double: asNumber * 2,
-          }
-        },
-      }),
-  )
-  .command(
-    new MassargCommand<{ component: string }>({
-      name: "remove",
-      description: "Remove a component",
-      aliases: ["r"],
-      run: (opts) => {
-        console.log("Removing component", opts.component)
-      },
-    }).option({
-      name: "component",
-      description: "Component to remove",
-      aliases: ["c"],
-      // aliases: "" as never,
-    }),
-  )
-  .option({
+  .command(addCmd)
+  .command(removeCmd)
+  .flag({
     name: "bool",
     description: "Example boolean option",
     aliases: ["b"],
-    type: "boolean",
   })
   .option({
     name: "number",
@@ -82,8 +82,6 @@ const args = massarg<A>({
     type: "number",
   })
 
-const opts = args.getArgs(process.argv.slice(2))
-
-console.log("Opts:", opts, "\n")
+// console.log("Opts:", args.getArgs(process.argv.slice(2)), "\n")
 
 args.parse(process.argv.slice(2))
---
+++ src/help.ts
@@ -0,0 +1,188 @@
+import { format, StringStyle, stripColors } from "./color"
+import MassargCommand from "./command"
+
+export type GenerateTableCommandConfig = {
+  maxRowLength?: number
+  namePrefix?: string
+  aliasPrefix?: string
+  compact?: boolean
+  nameStyle?: StringStyle
+  descriptionStyle?: StringStyle
+}
+
+export type GenerateTableOptionConfig = GenerateTableCommandConfig & {
+  typeStyle?: StringStyle
+  defaultStyle?: StringStyle
+}
+
+export type GenerateHelpOptions = {
+  // sub-styles
+  commandOptions?: GenerateTableCommandConfig
+  optionOptions?: GenerateTableOptionConfig
+
+  // global styles
+  titleStyle?: StringStyle
+  descriptionStyle?: StringStyle
+  subtitleStyle?: StringStyle
+  usageStyle?: StringStyle
+}
+
+export type HelpItem = {
+  name: string
+  aliases: string[]
+  description: string
+}
+
+export class HelpGenerator {
+  entry: MassargCommand<any>
+  config: GenerateHelpOptions
+
+  constructor(entry: MassargCommand<any>, config?: GenerateHelpOptions) {
+    this.entry = entry
+    this.config = {
+      commandOptions: {
+        ...config?.commandOptions,
+      },
+      optionOptions: {
+        ...config?.optionOptions,
+      },
+    }
+  }
+
+  generate(): string {
+    const entry = this.entry
+    const options = generateHelpTable(entry.options, {
+      namePrefix: "--",
+      aliasPrefix: "-",
+      ...this.config.optionOptions,
+    })
+    const commands = generateHelpTable(entry.commands, {
+      namePrefix: "",
+      aliasPrefix: "",
+      ...this.config.commandOptions,
+    })
+
+    return chainStr(
+      format(entry.name, {
+        bold: true,
+        color: "brightWhite",
+        reset: true,
+        ...this.config.titleStyle,
+      }),
+      "",
+      format(entry.description, { reset: true, ...this.config.descriptionStyle }),
+      commands.length && [
+        "",
+        format(`Commands for ${entry.name}:`, {
+          bold: true,
+          reset: true,
+          color: "brightWhite",
+          underline: true,
+          ...this.config.subtitleStyle,
+        }),
+        "",
+        commands,
+      ],
+      options.length && [
+        "",
+        format(`Options for ${entry.name}:`, {
+          bold: true,
+          reset: true,
+          color: "brightWhite",
+          underline: true,
+          ...this.config.subtitleStyle,
+        }),
+        "",
+        options,
+      ],
+    )
+  }
+
+  printHelp(): void {
+    console.log(this.generate())
+  }
+}
+
+function generateHelpTable<T extends Partial<GenerateTableCommandConfig>>(
+  items: HelpItem[],
+  {
+    maxRowLength = 80,
+    namePrefix = "",
+    aliasPrefix = "",
+    compact = false,
+    ...config
+  }: Partial<T> = {},
+): string {
+  const rows = items.map((o) => {
+    const name = `${namePrefix}${o.name}${
+      o.aliases.length ? ` | ${aliasPrefix}${o.aliases.join(`|${aliasPrefix}`)}` : ""
+    }`
+    const description = o.description
+    return { name, description }
+  })
+  const maxNameLength = Math.max(...rows.map((o) => o.name.length))
+  const nameStyle = (name: string) =>
+    format(name, { bold: true, color: "brightWhite", reset: true, ...config.nameStyle })
+  const descStyle = (desc: string) =>
+    format(desc, { color: "gray", reset: true, ...config.descriptionStyle })
+  const table = rows.map((row) => {
+    const name = nameStyle(row.name.padEnd(maxNameLength + 2))
+    const description = descStyle(row.description)
+    const length = stripColors(name).length + stripColors(description).length
+    if (length <= maxRowLength) {
+      const line = `${name}${description}`
+      if (!compact) {
+        return `${line}\n`
+      }
+      return line
+    }
+    const subRows: string[] = []
+    const words = description.split(" ")
+    let currentRow = name
+
+    for (const word of words) {
+      if (stripColors(currentRow).length + stripColors(word).length + 1 > maxRowLength) {
+        subRows.push(currentRow)
+        currentRow = " ".repeat(maxNameLength + 2)
+      }
+      currentRow += `${word} `
+    }
+
+    if (!compact) {
+      subRows.push("")
+    }
+
+    return subRows.join("\n")
+  })
+
+  return table.join("\n")
+}
+
+type Parseable = string | number | boolean | Record<string, unknown>
+
+function chainStr(...strs: (Parseable | Parseable[])[]) {
+  const res: string[] = []
+  for (const str of strs) {
+    if (typeof str === "string") {
+      res.push(str)
+      continue
+    }
+    if (Array.isArray(str)) {
+      res.push(chainStr(...str))
+      continue
+    }
+    if (typeof str === "object") {
+      for (const [key, value] of Object.entries(str)) {
+        if (Boolean(value)) {
+          res.push(key)
+        }
+      }
+      continue
+    }
+    if (Boolean(str)) {
+      res.push(str.toString())
+      continue
+    }
+  }
+  return res.join("\n")
+}
