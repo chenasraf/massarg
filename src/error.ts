34b351103cebf4aafd249f4287507e0b74fe07db
Chen Asraf <casraf@pm.me>
2023-11-27 22:25:58 +0200

    feat: different opt output name (default camelCase)

--- src/error.ts
+++ src/error.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod'
 
+export type ValidationErrorOptions = { path: string[]; code: string; message: string }
+
 /** This error is thrown when a validation fails. */
 export class ValidationError extends Error {
   /** The path to the value that failed validation. */
@@ -9,7 +11,7 @@ export class ValidationError extends Error {
   /** The error message. */
   message: string
 
-  constructor({ path, code, message }: { path: string[]; code: string; message: string }) {
+  constructor({ path, code, message }: ValidationErrorOptions) {
     const msg = `${path.join('.')}: ${message}`
     super(msg)
     this.path = path
@@ -17,6 +19,13 @@ export class ValidationError extends Error {
     this.message = msg
     this.name = 'ValidationError'
   }
+}
+
+export type ParseErrorOptions = {
+  path: string[]
+  code: string
+  message: string
+  received?: unknown
 }
 
 /** This error is thrown when a parse fails on an option value. */
@@ -30,17 +39,7 @@ export class ParseError extends Error {
   /** The value that failed parsing. */
   received: unknown
 
-  constructor({
-    path,
-    code,
-    message,
-    received,
-  }: {
-    path: string[]
-    code: string
-    message: string
-    received?: unknown
-  }) {
+  constructor({ path, code, message, received }: ParseErrorOptions) {
     let msg = `${path.join('.')}: ${message}`
     if (received) {
       msg += ` (received: ${received})`
